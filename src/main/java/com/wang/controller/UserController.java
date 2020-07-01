1c7a7d37419f6d0441c5456aad384dad399593a5
wang.xuejun3 <wang.xuejun3@iwhalecloud.com>
2020-07-01 11:38:44 +0800

    提交代码

--- src/main/java/com/wang/controller/UserController.java
+++ src/main/java/com/wang/controller/UserController.java
@@ -45,4 +45,5 @@ public class UserController {
         return "成功";
     }
 
+
 }
--- src/main/java/com/wang/utils/GlobalExceptionHandler.java
+++ src/main/java/com/wang/utils/GlobalExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.wang.utils;
 
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,5 +24,15 @@ public class GlobalExceptionHandler {
         return "我是个异常处理类";
     }
 
-
+    /**
+     * 错误访问处理
+     * @return
+     */
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> getWebServerFactoryCustomizer() {
+        return factory -> {
+            ErrorPage errorPage = new ErrorPage(HttpStatus.NOT_FOUND, "/404.do");
+            factory.addErrorPages(errorPage);
+        };
+    }
 }
